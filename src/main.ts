e3e1d0c352c9c8ba5a4307b2d365da7bbc2c3da1
SevenhuijsenM <64038833+SevenhuijsenM@users.noreply.github.com>
2025-03-02 15:33:11 +0100

    attempt to fix gh pages

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GLTFLoader, GLTF } from 'three/examples/jsm/loaders/GLTFLoader';
 
+console.log("hi")
 // Initialize Scene, Camera, and Renderer
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
